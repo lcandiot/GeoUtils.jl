e8a4ec525143ee59785345b8c55ba1157d9bb95d
Lorenzo Candioti <lorenzo.candioti@bluewin.ch>
2025-07-14 13:27:25 +0200

    Styling

--- src/VisualizationFunctions.jl
+++ src/VisualizationFunctions.jl
@@ -8,9 +8,11 @@ using CairoMakie, DocumenterVitepress
     update_CairoMakie_plot!(::Plot2D, plh::Heatmap{Tuple{Vector{Float64}, Vector{Float64}, Matrix{Float32}}}, z::AbstractArray)
 
 # Description:
+
 Updates a CairoMakie plot without emptying axis or recalling the plot itself. Arguments depend on the method selected.
 
 # Arguments:
+
 * `Plot1D()`, `Plot2D()` ...    : Structure that defines the type of plot
 * `plh`                         : Plot handle
 * `x`, `y`, `z`                 : Data used to update the plot
